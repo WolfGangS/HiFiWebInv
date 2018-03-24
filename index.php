39e1a19eb74845888e6fb239017fae99cfbce20d
WolfGang <flamin2k8@gmail.com>
2018-03-24 12:28:51 +0000

    blah

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<html>
-    <head>
-    </head>
-    <body>
-        <pre>
 <?php
-print_r($_SERVER);
-?>
-        </pre>
-    </body>
-</html>
+
+function curPageURL() {
+	$pageURL = 'http';
+	$https = false;
+	if ($_SERVER["HTTPS"] == "on") {
+		$pageURL .= "s";
+		$https = true;
+	}
+	$pageURL .= "://" . $_SERVER["SERVER_NAME"];
+	if ($_SERVER["SERVER_PORT"] != ($https ? "443" : "80")) {
+		$pageURL .= ":" . $_SERVER["SERVER_PORT"];
+	}
+	$pageURL .= $_SERVER["REQUEST_URI"];
+	return $pageURL;
+}
+
+$url = parse_url(curPageURL());
+
+print_r($url);
