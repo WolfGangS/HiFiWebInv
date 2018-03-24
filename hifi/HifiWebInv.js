27304f2a5bc0e119fc0a3a3401fedc49fe3bec84
WolfGang <flamin2k8@gmail.com>
2018-03-24 15:01:59 +0000

    blah

--- hifi/HifiWebInv.js
+++ hifi/HifiWebInv.js
@@ -1,3 +1,65 @@
-function boobs(){
-    print("boobs");
-}
+/*
+HiFiWebInv
+
+Created by Wolfgang
+on  30/9/2017
+
+ * @license
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+
+*/
+
+(function() {
+
+    var isActive = false;
+
+    var html = Script.resolvePath("html/inv.html?" + Date.now());
+    var inventory = new OverlayWebWindow({
+        title: "Editor",
+        source: html,
+        width: 500,
+        height: 800,
+        visible: false
+    });
+
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+    //print(JSON.stringify(Object.keys(tablet)));
+    var button = tablet.addButton({
+        icon: "https://wolfgangs.github.io/HiFiPowerEdit/spanner-white.svg",
+        activeIcon: "https://wolfgangs.github.io/HiFiPowerEdit/spanner.svg",
+        text: "Inventory",
+        isActive: false,
+        sortOrder: 30
+    });
+
+    function toggleWebState() {
+        setWebState(!isActive);
+    }
+
+    function setWebState(on) {
+        if (on === isActive) return;
+        isActive = on;
+        inventory.setVisible(isActive);
+        button.editProperties({ isActive: isActive });
+    }
+
+    button.clicked.connect(toggleWebState);
+
+    Script.scriptEnding.connect(function() {
+        //Controller.mouseReleaseEvent.disconnect(mouseReleaseEvent);
+        button.clicked.disconnect(toggleWebState);
+        tablet.removeButton(button);
+        editorUI.close();
+    });
+
+})();
