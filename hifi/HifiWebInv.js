57b8c5c1f3bae7177575aa32e03934e72de29a21
WolfGang <flamin2k8@gmail.com>
2018-03-24 15:09:24 +0000

    blah

--- hifi/HifiWebInv.js
+++ hifi/HifiWebInv.js
@@ -35,8 +35,8 @@ on  30/9/2017
     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
     //print(JSON.stringify(Object.keys(tablet)));
     var button = tablet.addButton({
-        icon: "https://wolfgangs.github.io/HiFiPowerEdit/spanner-white.svg",
-        activeIcon: "https://wolfgangs.github.io/HiFiPowerEdit/spanner.svg",
+        icon: "https://wolfgangs.github.io/HiFiWebInv/folder-white.svg",
+        activeIcon: "https://wolfgangs.github.io/HiFiWebInv/folder-black.svg",
         text: "Inventory",
         isActive: false,
         sortOrder: 30
