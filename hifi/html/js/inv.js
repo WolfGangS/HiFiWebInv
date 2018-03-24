f47433fce24843a849fa9bbed62908af8f51f40b
WolfGang <flamin2k8@gmail.com>
2018-03-24 17:25:37 +0000

    blah

--- hifi/html/js/inv.js
+++ hifi/html/js/inv.js
@@ -1,3 +1,18 @@
+function Api(endpoint){
+    var that = {};
+    that.endpoint = endpoint;
+
+    that.get = function(path){
+        $.get(endpoint + path,function(response){
+            console.log(response);
+        });
+    }
+}
+
+
+var api = null;
+
 $(document).ready(function(){
-    $("body").append($("<h2>").text(window.location.href));
+    api = new Api(window.location.href.split("/html/inv.html")[0]);
+    api.get();
 });
